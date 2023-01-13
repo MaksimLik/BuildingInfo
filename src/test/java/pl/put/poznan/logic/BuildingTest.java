f1ac9a0bdc3d115b788da472e3b92b5d2204f709
Nesia <helena.maslowska17@gmail.com>
2023-01-13 22:53:55 +0100

    add 2 tests of building and set basic environment

--- src/test/java/pl/put/poznan/logic/BuildingTest.java
+++ src/test/java/pl/put/poznan/logic/BuildingTest.java
@@ -1,23 +1,106 @@
 package pl.put.poznan.logic;
 
+import org.apache.tomcat.util.digester.Rule;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.testng.Assert;
+
+import java.io.ByteArrayOutputStream;
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class BuildingTest {
+    Building building1, building2, building3;
 
-    private Building building = null;
+    Floor floor1, floor2, floor3, floor4;
+    Room room1, room2, room3, room4, room5, room6, room7, room8, room9, room10;
+    ArrayList<Floor> building1Floors, building2Floors, building3Floors;
+
+    ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
     @BeforeEach
     public void setUp() throws Exception  {
-        building = new Building(1,"Rezydencja");
+        building1 = new Building(1,"Zamek");
+        building2 = new Building(2,"Willa");
+        building3 = new Building(3,"Schowek");
+
+        floor1 = new Floor(3, "Carpet");
+        floor2 = new Floor(4, "Restaurant");
+        floor3 = new Floor(5, "Saloon");
+        floor4 = new Floor(6, "Taras");
+
+        room1 = new Room(10, "Bathroom", 25, 85, 25, 40);
+        room2 = new Room(11, "Toilet", 15, 10, 10, 10);
+        room3 = new Room(12, "Kitchen", 40, 160, 45, 150);
+        room4 = new Room(13, "Bedroom", 20, 55, 30, 30);
+        room5 = new Room(14, "Living room", 125, 130, 215, 115);
+        room6 = new Room(15, "Painting room", 40, 120, 35, 50);
+
+        room7 = new Room(10, "Bathroom", 25, 85, 25, 40);
+        room8 = new Room(11, "Living room", 15, 50, 10, 10);
+        room9 = new Room(12, "Kitchen", 40, 160, 45, 150);
+        room10 = new Room(13, "Bedroom", 20, 55, 30, 30);
+
+        floor1.add(room1);
+        floor1.add(room2);
+        floor1.add(room3);
+        floor1.add(room4);
+        floor1.add(room5);
+        floor1.add(room6);
+
+        floor2.add(room7);
+        floor2.add(room8);
+        floor2.add(room9);
+        floor2.add(room10);
+        floor3.add(room2);
+        // floor4 has no rooms
+
+        building1.add(floor1);
+        building1.add(floor2);
+        building2.add(floor3);
+        building2.add(floor4);  //add empty floor
+
+        building3.add(floor3);
+
+        building1Floors = new ArrayList<Floor>();
+        building1Floors.add(floor1);
+        building1Floors.add(floor2);
+
+        building2Floors = new ArrayList<Floor>();
+        building2Floors.add(floor3);
+        building2Floors.add(floor4);
+
+        building3Floors = new ArrayList<Floor>();
+        building3Floors.add(floor3);
+
     }
 
+    /**
+     * Check sum of all floors
+     * first test: add 2 floors, first has 1 room, second don't have any
+     * second test: add single floor
+     */
     @Test
-    void sumArea() {
+    void testGetArea() {
+        assertEquals(365, building1.getArea()); //sum couple of rooms
+        assertEquals(15, building2.getArea()); //sum two floors, floor4 is empty
+        assertEquals(15, building3.getArea()); //sum single floor
     }
 
+    /**
+     * Pass when list is created correctly.
+     */
     @Test
-    void sumCube() {
+    void testGetFloors() {
+        Assert.assertEquals(building1Floors, building1.getFloors());
+        Assert.assertEquals(building2Floors, building2.getFloors());
+        Assert.assertEquals(building3Floors, building3.getFloors());
+    }
+
+
+    @Test
+    void testSumCube() {
+
     }
 }
