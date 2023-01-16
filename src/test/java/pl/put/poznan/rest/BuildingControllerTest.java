2899de82020824c9e74bfe9f206481fa7b77f266
Nesia <helena.maslowska17@gmail.com>
2023-01-16 22:30:45 +0100

    Start mock class

--- src/test/java/pl/put/poznan/rest/BuildingControllerTest.java
+++ src/test/java/pl/put/poznan/rest/BuildingControllerTest.java
@@ -1,4 +1,103 @@
+package pl.put.poznan.rest;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import pl.put.poznan.logic.Building;
+
 import static org.junit.jupiter.api.Assertions.*;
-class BuildingControllerTest {
-  
+import static org.mockito.Mockito.*;
+
+class BuildingControllerTest
+{
+    private BuildingController buildingController;
+    private BuildingService mockObject;
+    Building building;
+    
+    @BeforeEach
+    void setUp()
+    {
+        mockObject = mock(BuildingService.class);
+        buildingController = new BuildingController(mockObject);
+    }
+
+    @Test
+    void getBuildings()
+    {
+        when(mockObject.getBuildings()).thenReturn(building);
+
+        // Interakcja
+        TestedClass testedObject = new TestedClass(mockObject);
+        int result = testedObject.testedMethod("Something");
+
+        // Weryfikacja
+        // weryfikacja poprawności interakcji z obiektem zastępczym (Mockito)
+        verify(mockObject).someMethod("Something")
+        // weryfikacja kodu testowanego (JUnit)
+        assertEquals(1, result);
+    }
+
+    @Test
+    void getBuildingById()
+    {
+    }
+
+    @Test
+    void getFloorById()
+    {
+    }
+
+    @Test
+    void getRoomById()
+    {
+    }
+
+    @Test
+    void getOverheatedRooms()
+    {
+    }
+
+    @Test
+    void insertBuilding()
+    {
+    }
+
+    @Test
+    void insertFloor()
+    {
+    }
+
+    @Test
+    void insertRoom()
+    {
+    }
+
+    @Test
+    void deleteBuildingById()
+    {
+    }
+
+    @Test
+    void deleteFloorById()
+    {
+    }
+
+    @Test
+    void deleteRoomById()
+    {
+    }
+
+    @Test
+    void updateBuilding()
+    {
+    }
+
+    @Test
+    void updateFloor()
+    {
+    }
+
+    @Test
+    void updateRoom()
+    {
+    }
 }
